77973461ec6882885aacfc30675ab93ca0bfa5b9
Leon Ingelse <leoningelse@gmail.com>
2021-10-28 15:34:50 +0100

    Update number_maker.py

--- src/number_maker.py
+++ src/number_maker.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Annotated, List, Protocol, Tuple
+from typing import List, Protocol, Tuple
 from geneticengine.core.grammar import extract_grammar
 from geneticengine.core.representations.treebased import treebased_representation
 from geneticengine.core.tree import Node
