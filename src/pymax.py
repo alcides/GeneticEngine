851cfdb9f119eb2311b95d143355dc0c7426e52a
Leon Ingelse <leoningelse@gmail.com>
2021-11-02 14:00:45 +0000

    Updated pymax example to replicate PonyGE2 usage

--- src/pymax.py
+++ src/pymax.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from dataclasses import dataclass
 from enum import Enum
+from textwrap import indent
 from typing import Annotated, List, Protocol, Union
 from geneticengine.core.grammar import extract_grammar
 from geneticengine.core.representations.treebased import treebased_representation
@@ -8,6 +9,7 @@ from geneticengine.core.tree import Node
 from geneticengine.metahandlers.ints import IntRange
 from geneticengine.metahandlers.lists import ListSizeBetween
 from geneticengine.algorithms.gp.gp import GP
+from number_maker import Mul
 
 class Expr(Protocol):
     pass
@@ -20,20 +22,9 @@ class ForLoop(Node,Expr):
     def __init__(self,iterationRange,loopedCode):
         self.iterationRange = iterationRange
         self.loopedCode = loopedCode
-
-    def evaluate(self, **kwargs):
-        x = self.loopedCode
-        if x.__class__ == OneHalve:
-            y = deepcopy(x)
-        elif x.__class__ == ForLoop:
-            y = ForLoop(self.iterationRange * x.iterationRange, x.loopedCode)
-        else:
-            y = deepcopy(x)
-            if x.__class__ == PlusOneHalve:
-                for _ in range(self.iterationRange):
-                    # Add recursiveness
-                    y = Plus(deepcopy(x),deepcopy(y))
-        return y.evaluate()
+    
+    def __str__(self):
+        return 'for i in range({}):\n{}'.format(self.iterationRange,indent(str(self.loopedCode),'\t'))
 
 
             
@@ -45,55 +36,30 @@ class OneHalve(Node,Expr):
     def __str__(self) -> str:
         return "0.5"
 
-@dataclass
-class Plus(Node,Expr):
-    left: Node
-    right: Node
 
-    def __init__(self,left,right):
-        self.left = left
-        self.right = right
-    
-    def evaluate(self, **kwargs):
-        return self.left.evaluate() + self.right.evaluate()
-
-    def __str__(self) -> str:
-        return "(" + str(self.left) + " + " + str(self.right) + ")"
-
-@dataclass
 class PlusOneHalve(Node,Expr):
-    left: Node
-
-    def __init__(self,left):
-        self.left = left
     
-    def evaluate(self, **kwargs):
-        return self.left.evaluate() + OneHalve().evaluate()
-
     def __str__(self) -> str:
-        return "(" + str(self.left) + " + " + str(OneHalve()) + ")"
+        return "x = (" + 'x' + " + " + str(OneHalve()) + ")"
 
-
-@dataclass
-class Mult(Node,Expr):
-    left: Node
-
-    def __init__(self,left):
-        self.left = left
+class MultOneHalve(Node,Expr):
     
-    def evaluate(self, **kwargs):
-        return self.left.evaluate() * OneHalve().evaluate()
-
     def __str__(self) -> str:
-        return "(" + str(self.left) + " * " + str(OneHalve()) + ")"
+        return "x = (" + 'x' + " * " + str(OneHalve()) + ")"
 
 
 
+def fit(indiv):
+    code = 'x = 0\n' + str(indiv)
+    loc = {}
+    exec(code, globals(), loc)
+    x = loc['x']
+    return x
 
-fitness_function = lambda x: x.evaluate()
+fitness_function = lambda x: fit(x)
 
 if __name__ == "__main__":
-    g = extract_grammar([OneHalve,PlusOneHalve,ForLoop],Expr)
+    g = extract_grammar([PlusOneHalve,MultOneHalve,ForLoop],Expr)
     alg = GP(
         g,
         treebased_representation,
@@ -104,5 +70,6 @@ if __name__ == "__main__":
         number_of_generations=3,
         minimize=False
     )
-    (b,bf) = alg.evolve()
-    print(b,bf)
+    (b,bf) = alg.evolve(verbose=1)
+    print(b)
+    print("With fitness: {}".format(bf))
