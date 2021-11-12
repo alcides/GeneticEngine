19a70c3a5083e721072eedac632c2ed6e12ace5e
Leon Ingelse <leoningelse@gmail.com>
2021-11-12 17:16:34 +0100

    Added genetic engine path to allow genetic engine module import

--- examples/regression_example.py
+++ examples/regression_example.py
@@ -1,3 +1,7 @@
+import sys
+# Necessary to allow geneticengine import
+sys.path.insert(1, r'C:\\Users\\leoni\\Desktop\\Master\\Scriptie\\GeneticEngine')
+
 from typing import Annotated, Any, Callable
 
 import numpy as np
