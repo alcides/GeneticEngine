f12c7f8e98fee96ba198dfe4d36de1f77fde0fc5
Leon Ingelse <leoningelse@gmail.com>
2021-11-17 12:17:21 +0000

    Restructured folder

--- examples/progsys/Number IO.py
+++ examples/progsys/Number IO.py
@@ -1,8 +1,8 @@
-import os
-import ast
 from typing import Annotated, Any, Callable
 import sys
 sys.path.insert(1, 'C:\\Users\\leoni\\Desktop\\Master\\Scriptie\\GeneticEngine')
+
+from utils import get_data, import_embedded
 
 from geneticengine.core.grammar import extract_grammar
 from geneticengine.grammars.sgp import Plus, Literal, Number, Mul, SafeDiv, Var
@@ -10,36 +10,12 @@ from geneticengine.metahandlers.vars import VarRange
 from geneticengine.algorithms.gp.gp import GP
 from geneticengine.core.representations.treebased import treebased_representation
 
-
-
-CWD = os.path.dirname(os.path.realpath(__file__))
 FILE_NAME = "Number IO"
 DATA_FILE_TRAIN = "C:\\Users\\leoni\\Desktop\\Master\\Scriptie\\GeneticEngine\\examples\\progsys\\data\\{}\\Train.txt".format(FILE_NAME)
 DATA_FILE_TEST = "C:\\Users\\leoni\\Desktop\\Master\\Scriptie\\GeneticEngine\\examples\\progsys\\data\\{}\\Test.txt".format(FILE_NAME)
 
-def bla():
-    return "print(1 + 1)\nprint(2.5 * 2)"
-
-def get_data(data_file_train,data_file_test):
-    with open(data_file_train, 'r') as train_file, \
-            open(data_file_test, 'r') as test_file:
-        train_data = train_file.read()
-        test_data = test_file.read()
-
-    t = train_data.split('\n')
-
-    inval = t[0].strip('inval = ')
-    outval = t[1].strip('outval = ')
-    inval = ast.literal_eval(inval)
-    outval = ast.literal_eval(outval)
-    return inval,outval
-
-
 inval,outval = get_data(DATA_FILE_TRAIN,DATA_FILE_TEST)
-imported = __import__(FILE_NAME + "-Embed")
-
-evolved_function = lambda x,y: x+y
-
+imported = import_embedded(FILE_NAME)
 
 Var.__annotations__["name"] = Annotated[str, VarRange(["in0", "in1"])]
 g = extract_grammar([Plus, Mul, SafeDiv, Literal, Var], Number)
@@ -83,6 +59,4 @@ alg = GP(
 )
 (b, bf, bp) = alg.evolve(verbose=0)
 print(bf, bp, b)
-
-
 
