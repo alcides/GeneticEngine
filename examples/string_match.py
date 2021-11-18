90ad9e7717a66fb2f68e6dd8c39316ba950e801a
Paulo Alexandre Canelas dos Santos <psantos@lasige.di.fc.ul.pt>
2021-11-18 15:52:02 +0000

    Example now working. Updated grammars.

--- examples/string_match.py
+++ examples/string_match.py
@@ -1,105 +1,12 @@
-import re
-import string
-from abc import ABC
-from dataclasses import dataclass
-from textwrap import indent
-from typing import Annotated
-
-from geneticengine.metahandlers.vars import VarRange
-from geneticengine.core.grammar import extract_grammar
+from geneticengine.grammars.letter import *
 from geneticengine.algorithms.gp.gp import GP
+from geneticengine.core.grammar import extract_grammar
 from geneticengine.core.representations.treebased import treebased_representation
-'''
-Auxiliary lists of letters
-'''
-lower_vowel = ['a', 'e', 'i', 'o', 'u']
-upper_vowel = ['A', 'E', 'I', 'O', 'U']
-lower_consonant = list(re.sub('a|e|i|o|u', '', string.ascii_lowercase))
-upper_consonant = list(re.sub('A|E|I|O|U', '', string.ascii_uppercase))
-
-
-# string :: letter | letter string
-@dataclass
-class String(ABC):
-    pass
-
-
-# letter ::= char | vowel | consonant
-@dataclass
-class Letter(String):
-    pass
-
-
-# letter string
-@dataclass
-class LetterString(String):
-    letter: Letter
-    string: String
-
-    def __str__(self):
-        return str(self.letter) + str(self.string)
-
-
-# char ::= " " | ! | ? | , | .
-@dataclass
-class Char(Letter):
-    value: Annotated[str, VarRange([" ", "!", "?", ",", "."])]
-
-    def __str__(self):
-        return self.value
-
-
-# vowel ::= lower_vowl | upper_vowel
-# lower_vowel = a | e | i | o | u
-# upper_vowel = A | E | I | O | U
-@dataclass
-class Vowel(Letter):
-    pass
-
-
-@dataclass
-class LowerVowel(Vowel):
-    value: Annotated[str, VarRange(lower_vowel)]
-
-    def __str__(self):
-        return self.value
-
-
-@dataclass
-class UpperVowel(Vowel):
-    value: Annotated[str, VarRange(upper_vowel)]
-
-    def __str__(self):
-        return self.value
-
-
-# consonant ::= lower_consonant | upper_consonant
-# lower_consonant ::= b | c | d | f | g | h | j | k | l | ...
-# upper_consonant ::= B | C | D | F | G | H | J | K | L | ...
-@dataclass
-class Consonant(Letter):
-    pass
-
-
-@dataclass
-class LowerConsonant(Consonant):
-    value: Annotated[str, VarRange(lower_consonant)]
-
-    def __str__(self):
-        return self.value
-
-
-@dataclass
-class UpperConsonant(Consonant):
-    value: Annotated[str, VarRange(upper_consonant)]
-
-    def __str__(self):
-        return self.value
 
 
 def fit(individual: String):
     guess = str(individual)
-    target = 'OLA'
+    target = 'Hello world!'
     fitness = max(len(target), len(guess))
     # Loops as long as the shorter of two strings
     for (t_p, g_p) in zip(target, guess):
@@ -115,24 +22,19 @@ def fit(individual: String):
 fitness_function = lambda x: fit(x)
 
 if __name__ == "__main__":
-    g = extract_grammar([
-        LetterString, Char, LowerVowel, UpperVowel, LowerConsonant,
-        UpperConsonant
-    ], String)
+    g = extract_grammar([LetterString, Char, Vowel, Consonant], String)
     alg = GP(
         g,
         treebased_representation,
         fitness_function,
-        max_depth=10,
-        population_size=100,
-        number_of_generations=100,
+        max_depth=17,
+        population_size=500,
+        number_of_generations=500,
+        probability_crossover=0.75,
+        selection_method=("tournament", 2),
         minimize=True,
     )
     print("Started running...")
     (b, bf, bp) = alg.evolve(verbose=0)
-    print(bp)
-    print(b)
-    print("---")
-    print(str(bp))
-    print(str(b))
-    print("With fitness: {}".format(bf))
+    print(f"Best individual: {b}")
+    print(f"With fitness: {bf}")
