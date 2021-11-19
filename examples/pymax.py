38b821502de9c414959515c42d498113b8495204
Leon Ingelse <leoningelse@gmail.com>
2021-11-19 17:28:52 +0000

    Update pymax to import from grammar files

--- examples/pymax.py
+++ examples/pymax.py
@@ -1,35 +1,10 @@
-from abc import ABC
 from dataclasses import dataclass
 from textwrap import indent
-from typing import Annotated, List, NamedTuple, Protocol
 from geneticengine.core.grammar import extract_grammar
 from geneticengine.core.representations.treebased import treebased_representation
-from geneticengine.metahandlers.ints import IntRange
-from geneticengine.metahandlers.lists import ListSizeBetween
 from geneticengine.algorithms.gp.gp import GP
-
-
-class Statement(ABC):
-    def evaluate(self, x: float) -> float:
-        return x
-
-
-class Expr(ABC):
-    def evaluate(self, x: float) -> float:
-        return 0.0
-
-
-@dataclass
-class Code(Statement):
-    stmts: List[Statement]
-
-    def evaluate(self, x: float = 1.0) -> float:
-        for stmt in self.stmts:
-            x = stmt.evaluate(x)
-        return x
-
-    def __str__(self):
-        return "\n".join([str(stmt) for stmt in self.stmts])
+from geneticengine.grammars.control_flow import Statement, Code, ForLoop
+from geneticengine.grammars.conditions import Expr
 
 
 @dataclass
@@ -41,23 +16,6 @@ class XAssign(Statement):
 
     def __str__(self):
         return "x = {}".format(self.value)
-
-
-@dataclass
-class ForLoop(Statement):
-    iterationRange: Annotated[int, IntRange(1, 6)]
-    loopedCode: Statement
-
-    def evaluate(self, x: float = 1.0) -> float:
-        for _ in range(self.iterationRange):
-            x = self.loopedCode.evaluate(x)
-        return x
-
-    def __str__(self):
-        return "for i in range({}):\n{}".format(
-            self.iterationRange, indent(str(self.loopedCode), "\t")
-        )
-
 
 class VarX(Expr):
     def evaluate(self, x=0):
@@ -113,7 +71,7 @@ if __name__ == "__main__":
         fitness_function,
         max_depth=10,
         population_size=40,
-        number_of_generations=5,
+        number_of_generations=15,
         minimize=False,
     )
     (b, bf, bp) = alg.evolve(verbose=0)
