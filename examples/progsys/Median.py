7188be69ee71ed53114706d06403a0de7c0f7c46
Leon Ingelse <leoningelse@gmail.com>
2021-11-20 18:40:52 +0000

    Median takes too long

--- examples/progsys/Median.py
+++ examples/progsys/Median.py
@@ -41,7 +41,7 @@ alg = GP(
     g,
     treebased_representation,
     fitness_function,
-    number_of_generations=100,
+    number_of_generations=10,
     minimize=True,
 )
 (b, bf, bp) = alg.evolve(verbose=0)
