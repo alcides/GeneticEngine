84e24afe9a17e161c9297f183a5f0e73fc08e70c
Leon Ingelse <leoningelse@gmail.com>
2021-11-22 16:28:54 +0000

    Created grammar.coding.lists module

--- geneticengine/grammars/coding/classes.py
+++ geneticengine/grammars/coding/classes.py
@@ -29,7 +29,7 @@ class Condition(Expr):
     pass
 
 @abstract
-class List(Expr):
+class NumberList(Expr):
     pass
 
 @dataclass
---
+++ geneticengine/grammars/coding/lists.py
@@ -0,0 +1,113 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import Annotated, Callable, Any, List
+from geneticengine.exceptions import GeneticEngineError
+from geneticengine.grammars.coding.classes import Expr, Statement, NumberList, Number
+from geneticengine.metahandlers.vars import VarRange
+from geneticengine.metahandlers.ints import IntRange
+
+
+@dataclass
+class Max(Number):
+    list: NumberList
+
+    def evaluate(self, **kwargs):
+        return max(self.list.evaluate(**kwargs))
+    
+    def evaluate_lines(self, **kwargs) -> Callable[[Any], float]:
+        return lambda line: max(self.list.evaluate_lines(**kwargs)(line))
+
+    def __str__(self) -> str:
+        return f"max({self.list})"
+
+@dataclass
+class Min(Number):
+    list: NumberList
+
+    def evaluate(self, **kwargs):
+        return min(self.list.evaluate(**kwargs))
+    
+    def evaluate_lines(self, **kwargs) -> Callable[[Any], float]:
+        return lambda line: min(self.list.evaluate_lines(**kwargs)(line))
+
+    def __str__(self) -> str:
+        return f"min({self.list})"
+
+@dataclass
+class Length(Number):
+    list: NumberList
+
+    def evaluate(self, **kwargs):
+        return len(self.list.evaluate(**kwargs))
+    
+    def evaluate_lines(self, **kwargs) -> Callable[[Any], float]:
+        return lambda line: len(self.list.evaluate_lines(**kwargs)(line))
+
+    def __str__(self) -> str:
+        return f"len({self.list})"
+
+@dataclass
+class Combine(NumberList):
+    list1: NumberList
+    list2: NumberList
+
+    def evaluate(self, **kwargs):
+        return self.list1.evaluate(**kwargs) + self.list2.evaluate(**kwargs)
+    
+    def evaluate_lines(self, **kwargs) -> Callable[[Any], float]:
+        return lambda line: self.list1.evaluate_lines(**kwargs)(line) + self.list2.evaluate_lines(**kwargs)(line)
+
+    def __str__(self) -> str:
+        return f"({self.list1} + {self.list2})"
+
+
+@dataclass
+class Literal(NumberList):
+    list: List[Number]
+
+    def evaluate(self, **kwargs):
+        return [ v.evaluate(**kwargs) for v in self.list ]
+    
+    def evaluate_lines(self, **kwargs):
+        return lambda line: [ v.evaluate(**kwargs)(line) for v in self.list ]
+    
+    def __str__(self) -> str:
+        return str(self.list)
+
+
+@dataclass
+class GetElement(Number):
+    list: NumberList
+    element: Number
+
+    def evaluate(self, **kwargs):
+        list_length = Length(self.list).evaluate(**kwargs)
+        return self.list.evaluate(**kwargs)[self.element.evaluate(**kwargs) % list_length]
+    
+    def evaluate_lines(self, **kwargs):
+        list_length = lambda line: Length(self.list).evaluate_lines(**kwargs)(line)
+        return lambda line: self.list.evaluate_lines(**kwargs)(line)[self.element.evaluate_lines(**kwargs)(line) % list_length(line)]
+    
+    def __str__(self) -> str:
+        return str(self.val)
+    
+    
+@dataclass
+class Var(NumberList):
+    name: Annotated[str, VarRange(["x", "y", "z"])]
+
+    def evaluate(self, **kwargs):
+        return kwargs[self.name]
+    
+    def evaluate_lines(self, **kwargs):
+        if not hasattr(self,"feature_indices"):
+            raise GeneticEngineError("To use geneticengine.grammars.coding.lists.Var.evaluate_lines, one must specify a Var.feature_indices dictionary.")
+        return lambda line: line[self.feature_indices[self.name]]
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+import geneticengine.grammars.coding.numbers as numbers
+import IPython as ip
+ip.embed()
