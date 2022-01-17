cfae61ecc9082b5dd4cfd1804e04204e662e5148
Alcides Fonseca <me@alcidesfonseca.com>
2022-01-17 22:18:04 +0000

    Added documentation to MetaHandler

--- geneticengine/metahandlers/base.py
+++ geneticengine/metahandlers/base.py
@@ -6,6 +6,8 @@ from geneticengine.core.grammar import Grammar
 
 
 class MetaHandlerGenerator(Protocol):
+    """MetaHandlers are type refinements. They override the generation procedure of the base type."""
+
     def generate(
         self,
         r: Source,
@@ -16,4 +18,15 @@ class MetaHandlerGenerator(Protocol):
         argname: str,
         context: Dict[str, Type],
     ):
+        """
+        Generates an instance of type base_type, according to some criterion.
+
+        :param Source r: Random source for generation
+        :param Grammar g: Grammar to follow in the generation
+        :param Callable[[int, Type], Any] rec: The method to generate a new instance of type and maximum depth d
+        :param int depth: the current depth budget
+        :param Type base_type: The inner type being annotated
+        :param str argname: The name of the field of the parent object which is being generated
+        :param Dict[str, Type] context: The names and types of all fields in the parent object
+        """
         ...
