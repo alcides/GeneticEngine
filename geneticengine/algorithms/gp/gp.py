c7a5d369f8c54425a33ad4881042f5dd20f9b0a2
Leon Ingelse <leoningelse@gmail.com>
2022-01-20 10:49:29 +0000

    Individual name change

--- geneticengine/algorithms/gp/gp.py
+++ geneticengine/algorithms/gp/gp.py
@@ -12,7 +12,7 @@ from geneticengine.core.grammar import Grammar
 from geneticengine.core.random.sources import RandomSource
 from geneticengine.core.representations.api import Representation
 from geneticengine.core.representations.tree.treebased import treebased_representation
-from geneticengine.algorithms.gp.individual import Individual
+from geneticengine.algorithms.gp.Individual import Individual
 import geneticengine.algorithms.gp.generation_steps.selection as selection
 import geneticengine.algorithms.gp.generation_steps.mutation as mutation
 import geneticengine.algorithms.gp.generation_steps.cross_over as cross_over
