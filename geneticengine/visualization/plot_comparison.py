ffe9851036926ad59b1c7e319b562552c37fd653
Leon Ingelse <leoningelse@gmail.com>
2022-01-21 10:48:11 +0000

    Updated visualization to allow for more run inputs

--- geneticengine/visualization/plot_comparison.py
+++ geneticengine/visualization/plot_comparison.py
@@ -3,6 +3,8 @@ import pandas as pd
 from pylab import *
 from palettable.colorbrewer.qualitative import Set2_7
 
+from geneticengine.exceptions import GeneticEngineError
+
 def load(example_name, metric, single_value):
     f_list = glob.glob(f'results\csvs\{example_name}_(seed=*).csv')
 
@@ -33,33 +35,36 @@ def perc(data):
    return median, perc_25, perc_75
     
 
-def plot_comparison(file_name1, file_name2, name1, name2, result_name='results/images/medians.png', metric='fitness', single_value=False):
-    example1_data = load(file_name1,metric=metric,single_value=single_value)
-    example2_data = load(file_name2,metric=metric,single_value=single_value)
-
-    n_generations = example1_data.shape[1]
-    x = np.arange(0, n_generations)
-
-    med_example1, perc_25_example1, perc_75_example1 = perc(example1_data)
-    med_example2, perc_25_example2, perc_75_example2 = perc(example2_data)
-
+def plot_comparison(file_run_names, run_names, result_name='results/images/medians.png', metric='fitness', single_value=False):
+    if len(file_run_names) != len(run_names) and len(run_names) != 0:
+        raise GeneticEngineError('The given [file_run_names] has a different length than the given [run_names]. Length should be same or keep enter an empty list for [run_names].')
+    if len(run_names) == 0:
+        print('[run_names] is empty. Taking [file_run_names] as run_names.')
+        run_names = file_run_names
 
     colors = Set2_7.mpl_colors
     axes(frameon=0)
     grid(axis='y', color="0.9", linestyle='-', linewidth=1)
+    line_styles = ['solid', 'dotted', 'dashed', 'dashdot']
+    
+    for idx, file_run_name in enumerate(file_run_names):
+        run_data = load(file_run_name,metric=metric,single_value=single_value)
 
-    plot(x, med_example1, linewidth=2, color=colors[0])
-    plot(x, med_example2, linewidth=2, linestyle='--', color=colors[1])
+        n_generations = run_data.shape[1]
+        x = np.arange(0, n_generations)
 
-    fill_between(x, perc_25_example1, perc_75_example1, alpha=0.25, linewidth=0, color=colors[0])
-    fill_between(x, perc_25_example2, perc_75_example2, alpha=0.25, linewidth=0, color=colors[1])
+        med_run_data, perc_25_run_data, perc_75_run_data = perc(run_data)
 
-    legend = plt.legend([name1, name2], loc=4)
+        plot(x, med_run_data, linewidth=2, color=colors[idx % len(colors)], linestyle=line_styles[idx % len(line_styles)])
+
+        fill_between(x, perc_25_run_data, perc_75_run_data, alpha=0.25, linewidth=0, color=colors[idx % len(colors)], label='_nolegend_')
+
+    legend = plt.legend(run_names, loc=4)
     frame = legend.get_frame()
     frame.set_facecolor('1.0')
     frame.set_edgecolor('1.0')
 
     savefig(result_name)
 
-plot_comparison('all\GoL', 'all\GoL_vectorial', 'normal GoL', 'vectorial GoL')
+plot_comparison(['all\GoL', 'all\GoL_vectorial'], ['normal GoL', 'vectorial GoL'])
 
