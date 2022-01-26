f9eb718fe82a6aa9af456b6ab8ebfde2f6b963b1
Leon Ingelse <leoningelse@gmail.com>
2022-01-26 00:51:49 +0000

    Update plot_comparison.py

--- geneticengine/visualization/plot_comparison.py
+++ geneticengine/visualization/plot_comparison.py
@@ -6,7 +6,10 @@ from palettable.colorbrewer.qualitative import Set2_7
 from geneticengine.exceptions import GeneticEngineError
 
 def load(example_name, metric, single_value):
-    f_list = glob.glob(f'results\csvs\{example_name}_(seed=*).csv')
+    search_folder = f'results\csvs\{example_name}_seed=*.csv'
+    print(search_folder)
+    f_list = glob.glob(search_folder)
+    print(f_list)
 
     data = list()
 
@@ -49,7 +52,6 @@ def plot_comparison(file_run_names, run_names, result_name='results/images/media
     
     for idx, file_run_name in enumerate(file_run_names):
         run_data = load(file_run_name,metric=metric,single_value=single_value)
-
         n_generations = run_data.shape[1]
         x = np.arange(0, n_generations)
 
@@ -66,5 +68,37 @@ def plot_comparison(file_run_names, run_names, result_name='results/images/media
 
     savefig(result_name)
 
-plot_comparison(['all\GoL', 'all\GoL_vectorial'], ['normal GoL', 'vectorial GoL'])
+def plot_comparison2(file_name1, file_name2, name1, name2, result_name='results/images/medians2.png', metric='fitness', single_value=False):
+    example1_data = load(file_name1,metric=metric,single_value=single_value)
+    example2_data = load(file_name2,metric=metric,single_value=single_value)
+
+    n_generations = example1_data.shape[1]
+    x = np.arange(0, n_generations)
+
+    med_example1, perc_25_example1, perc_75_example1 = perc(example1_data)
+    med_example2, perc_25_example2, perc_75_example2 = perc(example2_data)
+    
+    print(med_example1)
+    print(med_example2)
+    
+    colors = Set2_7.mpl_colors
+    axes(frameon=0)
+    grid(axis='y', color="0.9", linestyle='-', linewidth=1)
+    
+    plot(x, med_example1, linewidth=2, color=colors[0])
+    plot(x, med_example2, linewidth=2, linestyle='--', color=colors[1])
+    
+    fill_between(x, perc_25_example1, perc_75_example1, alpha=0.25, linewidth=0, color=colors[0])
+    fill_between(x, perc_25_example2, perc_75_example2, alpha=0.25, linewidth=0, color=colors[1])
+    
+    legend = plt.legend([name1, name2], loc=4)
+    frame = legend.get_frame()
+    frame.set_facecolor('1.0')
+    frame.set_edgecolor('1.0')
+
+    savefig(result_name)
+    
+plot_comparison2('GoL\grammar_row\\run', 'GoL\grammar_standard\\run', 'normal GoL', 'vectorial GoL')
+
+# plot_comparison(['GoL\grammar_col\\run', 'GoL\grammar_standard\\run'], ['col', 'row'])
 
