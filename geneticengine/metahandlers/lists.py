396379a091445188b16ae15ec8b6515863e40a35
Alcides Fonseca <me@alcidesfonseca.com>
2022-01-31 18:12:07 +0000

    Metahandler for lists follows the list sketch

--- geneticengine/metahandlers/lists.py
+++ geneticengine/metahandlers/lists.py
@@ -26,12 +26,16 @@ class ListSizeBetween(MetaHandlerGenerator):
         new_symbol,
         depth: int,
         base_type,
-        context: Dict[str, str],
+        ctx: Dict[str, str],
     ):
-        size = r.randint(self.min, self.max)
+        
+        size = r.randint(0, depth - 1)
         fins = build_finalizers(lambda *x: rec(list(x)), size)
-        for i in range(size):
-            new_symbol(base_type, fins[i], depth - 1, f"{i}", context)
+        ident = ctx["_"]
+        for i, fin in enumerate(fins):
+            nctx = ctx.copy()
+            nctx["_"] = ident + "_" + str(i)
+            new_symbol(base_type, fin, depth - 1, ident, nctx)
 
     def __repr__(self):
         return f"ListSizeBetween[{self.min}...{self.max}]"
