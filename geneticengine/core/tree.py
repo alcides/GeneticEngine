181891f03d19cb90820bd1df472fcd57d3fc8d88
Guilherme Espada <gjespada@fc.ul.pt>
2022-03-14 15:35:39 +0000

    fix mypy

--- geneticengine/core/tree.py
+++ geneticengine/core/tree.py
@@ -1,4 +1,4 @@
-from typing import Protocol, runtime_checkable, List, Dict, Any
+from typing import Protocol, runtime_checkable, Set
 
 from geneticengine.core.utils import get_arguments
 
@@ -8,7 +8,7 @@ class TreeNode(Protocol):
     gengy_labeled: bool
     gengy_distance_to_term: int
     gengy_nodes: int
-    gengy_types_this_way: Dict[type, List[Any]]
+    gengy_types_this_way: Set[type]
 
 
 class PrettyPrintable(object):
