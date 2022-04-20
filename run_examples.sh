103c5f68514fd9615148cee56212d9c54bd3b2b4
Guilherme Espada <gjespada@fc.ul.pt>
2022-04-20 10:31:49 +0100

    Revert "Doesn't work"
    This reverts commit 4fee951463ef3bbdf3349e7dea75f234fcf83ac9.

---
+++ run_examples.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}."
+
+PYTHON_BINARY=python3
+
+
+
+function run_example {
+    printf "Running $1..."
+    $PYTHON_BINARY $1 > /dev/null && echo "(done)" || echo "(failed)"
+
+}
+
+# # Should be somewhere else (maybe add to unit tests)
+# run_example examples/simple_choice_of_choice.py
+
+run_example examples/example.py
+run_example examples/pymax.py # Works in PonyGE
+# run_example examples/vectorialgp_example.py
+run_example examples/regression.py # Works in PonyGE
+run_example examples/classification.py # Works in PonyGE
+run_example examples/santafe.py
+run_example examples/game_of_life.py
+run_example examples/string_match.py # Works in PonyGE
+
+run_example examples/progsys/Number_IO.py # Works in PonyGE
+run_example examples/progsys/Median.py # Works in PonyGE: PonyGE gives very bad results
+run_example examples/progsys/Smallest.py # Works in PonyGE: PonyGE gives very bad results
+run_example examples/progsys/Sum_of_Squares.py # Works in PonyGE: PonyGE gives very bad results
