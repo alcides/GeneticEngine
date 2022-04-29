8d5b683ea7493d140bf54fbf2c2b99a08a939d2a
Leon Ingelse <leoningelse@gmail.com>
2022-04-29 17:59:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,13 @@ from __future__ import annotations
 
 import setuptools
 
-setuptools.setup()
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "Readme.md").read_text()
+
+
+setuptools.setup(
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
