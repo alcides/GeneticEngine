7c7dbd0afa6eedd921372afe68e7121907992e33
Leon Ingelse <leoningelse@gmail.com>
2022-04-29 17:42:08 +0100

    Reduce number of generations progsys examples

--- examples/progsys/Sum_of_Squares.py
+++ examples/progsys/Sum_of_Squares.py
@@ -96,7 +96,7 @@ def evolve(g, seed, mode, representation):
         g,
         fitness_function,
         representation=representation,
-        number_of_generations=50,
+        number_of_generations=5,
         minimize=True,
         seed=seed,
         max_depth=17,
