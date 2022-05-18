256a67ca48ac56a3221690f05a05db84c5a2d74f
Leon Ingelse <leoningelse@gmail.com>
2022-05-18 18:26:37 +0100

    Fixing some type issues

--- geneticengine/core/representations/grammatical_evolution/structured_ge.py
+++ geneticengine/core/representations/grammatical_evolution/structured_ge.py
@@ -81,8 +81,8 @@ def crossover(
 
 
 class StructuredListWrapper(Source):
-    dna: dict[str, list[int]]
-    indexes: dict[str, int]
+    dna: dict[type, list[int]]
+    indexes: dict[type, type]
 
     def __init__(self, dna):
         self.dna = dna
@@ -91,12 +91,12 @@ class StructuredListWrapper(Source):
             indexes[k] = 0
         self.indexes = indexes
 
-    def randint(self, min, max, prod: str) -> int:
+    def randint(self, min: int, max: int, prod: str = None) -> int:
         self.indexes[prod] = (self.indexes[prod] + 1) % len(self.dna[prod])
         v = self.dna[prod][self.indexes[prod]]
         return v % (max - min + 1) + min
 
-    def random_float(self, min, max, prod: str) -> float:
+    def random_float(self, min: float, max: float, prod: str = None) -> float:
         k = self.randint(1, 100000000, prod)
         return 1 * (max - min) / k + min
 
