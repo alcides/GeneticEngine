54e58a7948a45f24d91851b4418e15bdc1cd88bb
Leon Ingelse <leoningelse@gmail.com>
2022-06-06 10:32:55 +0100

    Revert "Problems with is_terminal function"
    This reverts commit 7727710abb84ae9113127f6019820afaeabba226.

--- geneticengine/core/grammar.py
+++ geneticengine/core/grammar.py
@@ -252,7 +252,6 @@ class Grammar:
                         changed |= process_reachability(sym, prods)
                 else:
                     if is_terminal(sym, self.non_terminals):
-                        # not isinstance(i, list) shouldn't be necessary. It is necessary now as lists are considered terminals.
                         val = 1
                     else:
                         args = get_arguments(sym)
