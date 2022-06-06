103451b850b266f994f2b6e590ce01fde55c388e
Leon Ingelse <leoningelse@gmail.com>
2022-06-06 10:49:57 +0100

    Fixed grammar's distance to terminal counting of lists

--- geneticengine/core/grammar.py
+++ geneticengine/core/grammar.py
@@ -256,6 +256,22 @@ class Grammar:
                     ):
                         # not isinstance(i, list) shouldn't be necessary. It is necessary now as lists are considered terminals.
                         val = 1
+                    elif isinstance(sym, list):
+                        for s in sym:
+                            args = get_arguments(sym)
+                            assert args
+                            val = max(
+                                val,
+                                max(
+                                    1 + self.get_distance_to_terminal(argt)
+                                    for (_, argt) in args
+                                ),
+                            )
+
+                            changed |= process_reachability(
+                                sym,
+                                [argt for (_, argt) in args],
+                            )
                     else:
                         args = get_arguments(sym)
                         assert args
