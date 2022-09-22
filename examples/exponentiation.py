65a9f786cc9605bc45873ff1e901619b0b031722
Eduardo Madeira <eduardomadeira@MacBook-Pro-de-Eduardo.local>
2022-09-22 13:19:54 +0100

    exponentiation example added and initial development in lexicase selection algo

---
+++ examples/exponentiation.py
@@ -0,0 +1,133 @@
+from __future__ import annotations
+
+import os
+from dataclasses import dataclass
+from math import isinf
+from typing import Annotated
+from typing import Any
+from typing import Callable
+
+import numpy as np
+import pandas as pd
+
+from geneticengine.algorithms.gp.gp import GP
+from geneticengine.core.grammar import extract_grammar
+from geneticengine.core.representations.grammatical_evolution.ge import (
+    ge_representation,
+)
+from geneticengine.core.representations.grammatical_evolution.structured_ge import (
+    sge_representation,
+)
+from geneticengine.core.representations.tree.treebased import treebased_representation
+from geneticengine.grammars.basic_math import SafeDiv
+from geneticengine.grammars.basic_math import SafeLog
+from geneticengine.grammars.basic_math import SafeSqrt
+from geneticengine.grammars.sgp import Minus
+from geneticengine.grammars.sgp import Literal
+from geneticengine.grammars.sgp import Mul
+from geneticengine.grammars.sgp import Number
+from geneticengine.grammars.sgp import Plus
+from geneticengine.grammars.sgp import Var
+from geneticengine.metahandlers.ints import IntRange
+from geneticengine.metahandlers.vars import VarRange
+from geneticengine.metrics import mse
+
+DATASET_NAME = "Vladislavleva4"
+DATA_FILE_TRAIN = f"data/{DATASET_NAME}/Train.txt"
+DATA_FILE_TEST = f"data/{DATASET_NAME}/Test.txt"
+
+bunch = pd.read_csv(DATA_FILE_TRAIN, delimiter="\t")
+target = bunch.response
+data = bunch.drop(["response"], axis=1)
+
+feature_names = list(data.columns.values)
+feature_indices = {}
+for i, n in enumerate(feature_names):
+    feature_indices[n] = i
+
+# Prepare Grammar
+Var.__init__.__annotations__["name"] = Annotated[str, VarRange(feature_names)]
+Var.feature_indices = feature_indices  # type: ignore
+
+
+@dataclass
+class Exponentiation(Number):
+    baseNumber: Number
+    powerNumber: Number
+
+    def evaluate(self, **kwargs):
+        d1 = self.baseNumber.evaluate(**kwargs)
+        d2 = self.powerNumber.evaluate(**kwargs)
+        try:
+            return b1 ** b2
+        except:
+            return 1.0
+        return
+
+
+def preprocess():
+    return extract_grammar(
+        [
+            Plus,
+            Minus,
+            Mul,
+            SafeDiv,
+            Literal,
+            Var,
+            SafeSqrt,
+            Exponentiation,
+            SafeLog,
+        ],
+        Number,
+    )
+
+
+def fitness_function(n: Number):
+    X = data.values
+    y = target.values
+
+    variables = {}
+    for x in feature_names:
+        i = feature_indices[x]
+        variables[x] = X[:, i]
+
+    y_pred = n.evaluate(**variables)
+    # mse is used in PonyGE, as the error metric is not None!
+    fitness = mse(y_pred, y)
+    if isinf(fitness) or np.isnan(fitness):
+        fitness = 100000000
+    return fitness
+
+
+def evolve(
+    g,
+    seed,
+    mode,
+):
+    alg = GP(
+        g,
+        fitness_function,
+        representation=treebased_representation,
+        minimize=True,
+        # As in PonyGE2:
+        probability_crossover=0.75,
+        probability_mutation=0.01,
+        number_of_generations=50,
+        max_depth=30,
+        # max_init_depth=10,
+        population_size=500,
+        selection_method=("tournament", 2),
+        n_elites=5,
+        # ----------------
+        seed=seed,
+        timer_stop_criteria=mode,
+    )
+    (b, bf, bp) = alg.evolve(verbose=1)
+    return b, bf
+
+
+if __name__ == "__main__":
+    g = preprocess()
+    bf, b = evolve(g, 0, False)
+    print(b)
+    print(f"With fitness: {bf}")
--- geneticengine/algorithms/gp/generation_steps/selection.py
+++ geneticengine/algorithms/gp/generation_steps/selection.py
@@ -62,3 +62,81 @@ def create_novelties(
         return [create_individual(max_depth) for _ in range(n_novelties)]
 
     return novelties
+
+
+"""
+We have a population
+And we have a list cases that we have to shuffle
+
+Then in a While loop we verify if the population is greater than 1 
+and if we have at least one case. 
+
+if that's true we take a look at the the first element of cases list and 
+we check which individuals in the population have the best error value, and "discard" the others of that population
+After the while loop we check the length of our population, if it has only one element returns that element, otherwise returns a random element"""
+
+"""
+pool    1   2   3   4
+------------------------
+case1   2   2   4   8       
+case2   3   1   3   4
+case3   9   2   1   1
+
+population = [1, 2, 3, 4]
+cases = [[2, 2, 4, 8], [3, 1, 3, 4], [9, 2, 1, 1]]
+"""
+
+
+def create_lexicase(
+    minimize: True,
+) -> Callable[[RandomSource, list[Individual], int], list[Individual]]:
+    def lexicase(
+        r: RandomSource,
+        population: list[Individual],
+        n_winners: int,
+
+    ) -> list[Individual]:
+        candidates = population.copy()
+        n_cases = len(fitness(population[0]))
+        cases = r.shuffle(list(range(n_cases)))
+
+        #assert
+        winners = []
+
+        for _ in range(n_winners):
+            #assuming that there is not equal individuals in the population (not the best option)
+            #candidates = [x for x in population.copy() if x not in winners]
+            candidates_to_check = candidates.copy()
+
+            while len(candidates_to_check) > 1 and len(case) > 0:
+                new_candidates = list()
+                c = cases[0]
+                min_max_value = 0
+
+                for i in range(len(candidates_to_check)):
+                    checking_candidate = candidates_to_check[i]
+                    check_value = checking_candidate.fitness[c]
+
+                    if not new_candidates:
+                        min_max_value = check_value
+                        new_candidates.append(checking_candidate)
+
+                    elif ((check_value < min_max_value and minimize)
+                            or (check_value > min_max_value and not minimize)):
+                        new_candidates.clear()
+                        min_max_value = check_value
+                        new_candidates.append(checking_candidate)
+
+                    elif check_value == min_max_value:
+                        new_candidates.append(checking_candidate)
+
+                candidates_to_check = new_candidates.copy()
+                cases.remove(c)
+
+            winner = r.choice(candidates_to_check) if len(
+                candidates_to_check) > 1 else candidates_to_check[0]
+            winners.append(winner)
+            candidates.remove(winner)
+
+        return winners
+    return lexicase
