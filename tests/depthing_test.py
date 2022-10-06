35e1a34f8716ac57dfec7ab753a07a69ac2b3c0d
Leon Ingelse <leoningelse@gmail.com>
2022-10-06 12:29:36 +0200

    Update depthing_test.py

--- tests/depthing_test.py
+++ tests/depthing_test.py
@@ -25,8 +25,7 @@ class Leaf(Root):
 
 
 @dataclass
-class A(Root):
-    # y: list[int]
+class MiddleList(Root):
     z: Annotated[list[Root], ListSizeBetween(2, 3)]
 
 
@@ -52,16 +51,14 @@ class TestDepthing:
         x = random_node(r, g, 4, Root, method=PI_Grow)
         g: Grammar = extract_grammar([Concrete, Middle], Root, False)
         y = random_node(r, g, 4, Root, method=PI_Grow)
-        g: Grammar = extract_grammar([Concrete, Middle, A], Root, False)
+        g: Grammar = extract_grammar([Concrete, Middle, MiddleList], Root, False)
         z = random_node(r, g, 4, Root, method=PI_Grow)
         assert x.gengy_distance_to_term == 1
         assert x.gengy_nodes == 1
         assert y.gengy_distance_to_term == 4
         assert y.gengy_nodes == 4
-        # assert z.gengy_distance_to_term == 4
-        # assert z.gengy_nodes == 7
-        # import IPython as ip
-        # ip.embed()
+        assert z.gengy_distance_to_term == 4
+        assert z.gengy_nodes == 7
 
     def test_expansion_depthing(self):
         r = RandomSource(seed=1)
@@ -69,14 +66,11 @@ class TestDepthing:
         x = random_node(r, g, 4, Root, method=PI_Grow)
         g: Grammar = extract_grammar([Concrete, Middle], Root, True)
         y = random_node(r, g, 8, Root, method=PI_Grow)
+        g: Grammar = extract_grammar([Concrete, Middle, MiddleList], Root, True)
+        z = random_node(r, g, 8, Root, method=PI_Grow)
         assert x.gengy_distance_to_term == 2
         assert x.gengy_nodes == 2
         assert y.gengy_distance_to_term == 6
         assert y.gengy_nodes == 6
-        # import IPython as ip
-        # ip.embed()
-
-
-# a = TestDepthing
-# a.test_normal_depthing(a)
-# a.test_expansion_depthing(a)
+        assert z.gengy_distance_to_term == 5
+        assert z.gengy_nodes == 12
