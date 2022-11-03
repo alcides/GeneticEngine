cd1d378094bb7bd2627008d9ae756c3025893602
Leon Ingelse <leoningelse@gmail.com>
2022-11-03 14:47:10 +0100

    Update run_examples.sh

--- run_examples.sh
+++ run_examples.sh
@@ -22,18 +22,19 @@ function run_example {
 # run_example examples/simple_choice_of_choice.py
 
 run_example examples/example.py
-run_example examples/pymax.py # Works in PonyGE
+run_example examples/pymax.py
 run_example examples/vectorialgp_example.py
-run_example examples/regression.py # Works in PonyGE
-run_example examples/regression_lexicase.py # Works in PonyGE
-run_example examples/classification.py # Works in PonyGE
-run_example examples/classification_lexicase.py # Works in PonyGE
-run_example examples/sklearn-type-examples.py # Works in PonyGE
+run_example examples/regression.py
+run_example examples/regression_lexicase.py
+run_example examples/classification.py
+run_example examples/classification_lexicase.py
+run_example examples/classification_probabilisticGE.py
+run_example examples/sklearn-type-examples.py
 run_example examples/santafe.py
 run_example examples/game_of_life.py
-run_example examples/string_match.py # Works in PonyGE
+run_example examples/string_match.py
 
-run_example examples/progsys/Number_IO.py # Works in PonyGE
-run_example examples/progsys/Median.py # Works in PonyGE: PonyGE gives very bad results
-run_example examples/progsys/Smallest.py # Works in PonyGE: PonyGE gives very bad results
-run_example examples/progsys/Sum_of_Squares.py # Works in PonyGE: PonyGE gives very bad results
+run_example examples/progsys/Number_IO.py
+run_example examples/progsys/Median.py
+run_example examples/progsys/Smallest.py
+run_example examples/progsys/Sum_of_Squares.py
