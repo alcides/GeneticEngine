20d2c24485d679011d7e51668b91692f60d1838e
Leon Ingelse <leoningelse@gmail.com>
2022-11-10 11:08:30 +0100

    Update utils.py

--- geneticengine/visualization/utils.py
+++ geneticengine/visualization/utils.py
@@ -3,6 +3,7 @@ import glob
 import numpy as np
 import pandas as pd
 
+from geneticengine.exceptions import GeneticEngineError
 
 def load(folder_name: str, x_axis: str, y_axis: str, minimize: bool):
     print(f"Loading from: {folder_name}")
@@ -12,9 +13,14 @@ def load(folder_name: str, x_axis: str, y_axis: str, minimize: bool):
 
     for f in f_list:
         df = pd.read_csv(f)
-        df = df[[x_axis, y_axis]]
-        data.append(df)
-        
+        try:
+            df = df[[x_axis, y_axis]]
+            data.append(df)
+        except:
+            continue
+    
+    if not data:
+        raise GeneticEngineError(f"No files in folder {folder_name} have both columns ({x_axis} and {y_axis}). \n We recommend using the CSVCallback in the GP class to generate the csv files with the GP class parameter save_to_csv.")
     all_data = pd.concat(data, axis=0, ignore_index=True)
 
     if minimize:
