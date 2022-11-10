1bdb3ea1b514e636af78024e73a09a51c11ba7b0
Leon Ingelse <leoningelse@gmail.com>
2022-11-10 11:36:31 +0100

    Name changes

--- geneticengine/visualization/per_gen_comparisons.py
+++ geneticengine/visualization/per_gen_comparisons.py
@@ -16,23 +16,13 @@ from geneticengine.visualization.utils import *
 matplotlib.rcParams["pdf.fonttype"] = 42
 matplotlib.rcParams["ps.fonttype"] = 42
 
-
-def plot_comparison(
-    folder_names: list,
-    labels: list,
-    minimize: bool,
-    labels_name: str = "Labels",
-    x_axis: str = "Generations",
-    y_axis: str = "Fitness",
-    title: str = "Fitness comparison",
-    file_name=None,
-):
+def plot_comparison(folder_names: list, labels: list, labels_name: str = 'Labels', x_axis: str = 'Generations', y_axis: str = 'Fitness', title: str = 'Fitness comparison', file_name = None):
     assert len(folder_names) == len(labels)
 
     all_data = list()
 
     for idx, folder_name in enumerate(folder_names):
-        data = load(folder_name, x_axis, y_axis, minimize)
+        data = load(folder_name, x_axis, y_axis)
         data[labels_name] = labels[idx]
         all_data.append(data)
 
@@ -53,82 +43,24 @@ def plot_comparison(
         file_name = title.replace(" ", "_") + ".pdf"
     plt.savefig(file_name)
     print(f"Saved figure to {file_name}.")
-
-
-def plot_fitness_comparison(
-    folder_names: list,
-    labels: list,
-    minimize: bool,
-    labels_name: str = "Labels",
-    x_axis: str = "Generations",
-    y_axis: str = "Fitness",
-    title: str = "Fitness comparison",
-    file_name=None,
-):
-    plot_comparison(
-        folder_names=folder_names,
-        labels=labels,
-        minimize=minimize,
-        labels_name=labels_name,
-        x_axis=x_axis,
-        y_axis=y_axis,
-        title=title,
-        file_name=file_name,
-    )
-
-
-def plot_test_fitness_comparison(
-    folder_names: list,
-    labels: list,
-    minimize: bool,
-    labels_name: str = "Labels",
-    x_axis: str = "Generations",
-    y_axis: str = "Test fitness",
-    title: str = "Test fitness comparison",
-    file_name=None,
-):
-    plot_comparison(
-        folder_names=folder_names,
-        labels=labels,
-        minimize=minimize,
-        labels_name=labels_name,
-        x_axis=x_axis,
-        y_axis=y_axis,
-        title=title,
-        file_name=file_name,
-    )
-
-
-def plot_nodes_comparison(
-    folder_names: list,
-    labels: list,
-    minimize: bool,
-    labels_name: str = "Labels",
-    x_axis: str = "Generations",
-    y_axis: str = "Nodes",
-    title: str = "Nodes comparison",
-    file_name=None,
-):
+    
+def plot_fitness_comparison(folder_names: list, labels: list, labels_name: str = 'Labels', x_axis: str = 'Generations', y_axis: str = 'Fitness', title: str = 'Fitness comparison', file_name = None):
+    plot_comparison(folder_names=folder_names, labels=labels, labels_name=labels_name, x_axis=x_axis, y_axis=y_axis, title=title, file_name=file_name)
+    
+def plot_test_fitness_comparison(folder_names: list, labels: list, labels_name: str = 'Labels', x_axis: str = 'Generations', y_axis: str = 'Test fitness', title: str = 'Test fitness comparison', file_name = None):
+    plot_comparison(folder_names=folder_names, labels=labels, labels_name=labels_name, x_axis=x_axis, y_axis=y_axis, title=title, file_name=file_name)
+    
+def plot_nodes_comparison(folder_names: list, labels: list, labels_name: str = 'Labels', x_axis: str = 'Generations', y_axis: str = 'Nodes', title: str = 'Nodes comparison', file_name = None):
     raise GeneticEngineError("Not yet implemented")
-    # plot_comparison(folder_names=folder_names, labels=labels, minimize=minimize, labels_name=labels_name, x_axis=x_axis, y_axis=y_axis, title=title, file_name=file_name)
+    # plot_comparison(folder_names=folder_names, labels=labels, labels_name=labels_name, x_axis=x_axis, y_axis=y_axis, title=title, file_name=file_name)
+    
 
-
-def plot_prods_comparison(
-    folder_names: list,
-    labels: list,
-    minimize: bool,
-    labels_name: str = "Labels",
-    x_axis: str = "Generations",
-    optimum: str = "Fitness",
-    y_axis: str = "Nodes",
-    title: str = "Fitness comparison",
-    file_name=None,
-):
-
+def plot_prods_comparison(folder_names: list, labels: list, labels_name: str = 'Labels', x_axis: str = 'Generations', extra: str = 'Fitness', y_axis: str = 'Nodes', title: str = 'Fitness comparison', file_name = None):
+    
     all_data = list()
 
     for idx, folder_name in enumerate(folder_names):
-        data = load_w_different_optimum(folder_name, x_axis, optimum, minimize, y_axis)
+        data = load_w_extra(folder_name, x_axis, y_axis, extra)
         data[labels_name] = labels[idx]
         all_data.append(data)
 
