7a5a46a9f4b4ca63b628cb54cfa90731bea273c3
Alcides Fonseca <me@alcidesfonseca.com>
2022-11-19 21:30:18 +0000

    Changed the nodes and depth to use the phenotype

--- geneticengine/algorithms/callbacks/csv_callback.py
+++ geneticengine/algorithms/callbacks/csv_callback.py
@@ -54,12 +54,13 @@ class CSVCallback(Callback):
             pop = [gp.get_best_individual(gp.problem, population)]
         self.time = time
         for ind in pop:
-            if hasattr(ind.genotype, "gengy_distance_to_term"):
-                depth = ind.genotype.gengy_distance_to_term
+            phenotype = ind.get_phenotype()
+            if hasattr(phenotype, "gengy_distance_to_term"):
+                depth = phenotype.gengy_distance_to_term
             else:
                 depth = -1
-            if hasattr(ind.genotype, "gengy_nodes"):
-                nodes = ind.genotype.gengy_nodes
+            if hasattr(phenotype, "gengy_nodes"):
+                nodes = phenotype.gengy_nodes
             else:
                 nodes = -1
             row = [
