8287208d47c2f93975267edae90a75b609fe966c
Leon Ingelse <leoningelse@gmail.com>
2022-11-23 16:21:54 +0000

    New visualization

--- geneticengine/visualization/utils.py
+++ geneticengine/visualization/utils.py
@@ -30,7 +30,7 @@ def load(folder_name: str, x_axis: str, y_axis: str):
 
     return all_data
 
-def load_w_extra(folder_name: str, x_axis: str, y_axis:str, extra_column: str):
+def load_w_extra(folder_name: str, x_axis: str, y_axis:str, extra_column: list):
     print(f"Loading from: {folder_name}")
     f_list = glob.glob(f"{folder_name}/*.csv")
 
@@ -39,7 +39,7 @@ def load_w_extra(folder_name: str, x_axis: str, y_axis:str, extra_column: str):
     for f in f_list:
         df = pd.read_csv(f)
         try:
-            df = df[[x_axis, y_axis, extra_column]]
+            df = df[[x_axis, y_axis] + extra_column]
             data.append(df)
         except:
             continue
