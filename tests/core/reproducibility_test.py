51a6b56849e24a9d10cfb0f8e1a14f7f58950907
Alcides Fonseca <me@alcidesfonseca.com>
2023-07-21 12:10:36 +0100

    Added Randomness examples

--- tests/core/reproducibility_test.py
+++ tests/core/reproducibility_test.py
@@ -1,3 +1,4 @@
+import copy
 from dataclasses import dataclass
 
 import pytest
@@ -24,13 +25,112 @@ class X:
     j: int
 
 
-def test_random():
+def test_random_int():
     r = RandomSource(1)
     v1 = r.randint(0, 10)
     r = RandomSource(2)
     v2 = r.randint(0, 10)
     r = RandomSource(1)
     v3 = r.randint(0, 10)
+    assert v1 == v3
+    assert v1 != v2
+
+
+def test_random_bool():
+    r = RandomSource(1)
+    v1 = [r.random_bool() for _ in range(100)]
+    r = RandomSource(2)
+    v2 = [r.random_bool() for _ in range(100)]
+    r = RandomSource(1)
+    v3 = [r.random_bool() for _ in range(100)]
+    assert v1 == v3
+    assert v1 != v2
+
+
+def test_random_float():
+    r = RandomSource(1)
+    v1 = r.random_float(0, 10)
+    r = RandomSource(2)
+    v2 = r.random_float(0, 10)
+    r = RandomSource(1)
+    v3 = r.random_float(0, 10)
+    assert v1 == v3
+    assert v1 != v2
+
+
+def test_random_norm():
+    r = RandomSource(1)
+    v1 = [r.normalvariate(3, 4) for _ in range(100)]
+    r = RandomSource(2)
+    v2 = [r.normalvariate(3, 4) for _ in range(100)]
+    r = RandomSource(1)
+    v3 = [r.normalvariate(3, 4) for _ in range(100)]
+    assert v1 == v3
+    assert v1 != v2
+
+
+def test_random_choice():
+    r = RandomSource(1)
+    v1 = r.choice([n for n in range(0, 1000)])
+    r = RandomSource(2)
+    v2 = r.choice([n for n in range(0, 1000)])
+    r = RandomSource(1)
+    v3 = r.choice([n for n in range(0, 1000)])
+    assert v1 == v3
+    assert v1 != v2
+
+
+def test_random_choice_weighted():
+    r = RandomSource(1)
+    v1 = r.choice_weighted([n for n in range(0, 1000)], [n for n in range(0, 1000)])
+    r = RandomSource(2)
+    v2 = r.choice_weighted([n for n in range(0, 1000)], [n for n in range(0, 1000)])
+    r = RandomSource(1)
+    v3 = r.choice_weighted([n for n in range(0, 1000)], [n for n in range(0, 1000)])
+    assert v1 == v3
+    assert v1 != v2
+
+
+def test_random_shuffle():
+    original = [n for n in range(100)]
+
+    r = RandomSource(1)
+    v1 = copy.copy(original)
+    v1 = r.shuffle(v1)
+    v1 = r.shuffle(v1)
+
+    r = RandomSource(2)
+    v2 = copy.copy(original)
+    v2 = r.shuffle(v2)
+    v2 = r.shuffle(v2)
+
+    r = RandomSource(1)
+    v3 = copy.copy(original)
+    v3 = r.shuffle(v3)
+    v3 = r.shuffle(v3)
+
+    assert v1 == v3
+    assert v1 != v2
+
+
+def test_random_pop():
+    original = [n for n in range(100)]
+
+    r = RandomSource(1)
+    l1 = copy.copy(original)
+    v1 = r.pop_random(l1)
+    v1 += r.pop_random(l1)
+
+    r = RandomSource(2)
+    l2 = copy.copy(original)
+    v2 = r.pop_random(l2)
+    v2 += r.pop_random(l2)
+
+    r = RandomSource(1)
+    l3 = copy.copy(original)
+    v3 = r.pop_random(l3)
+    v3 += r.pop_random(l3)
+
     assert v1 == v3
     assert v1 != v2
 
