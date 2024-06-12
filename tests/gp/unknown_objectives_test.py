9cf25c34c3ff100a5de6b0a01a6b3c455c85cb03
Alcides Fonseca <me@alcidesfonseca.com>
2024-06-12 13:49:31 +0100

    Different tests for target and sametarget

--- tests/gp/unknown_objectives_test.py
+++ tests/gp/unknown_objectives_test.py
@@ -8,11 +8,11 @@ from geneticengine.algorithms.gp.gp import GeneticProgramming
 from geneticengine.algorithms.gp.operators.combinators import SequenceStep
 from geneticengine.algorithms.gp.operators.crossover import GenericCrossoverStep
 from geneticengine.algorithms.gp.operators.mutation import GenericMutationStep
-from geneticengine.algorithms.gp.operators.selection import LexicaseSelection, TournamentSelection
-from geneticengine.evaluation.budget import AnyOf, EvaluationBudget, TargetMultiFitness
+from geneticengine.algorithms.gp.operators.selection import LexicaseSelection
+from geneticengine.evaluation.budget import AnyOf, EvaluationBudget, TargetMultiFitness, TargetMultiSameFitness
 from geneticengine.grammar.grammar import extract_grammar
 from geneticengine.grammar.metahandlers.ints import IntRange
-from geneticengine.problems import MultiObjectiveProblem, SingleObjectiveProblem
+from geneticengine.problems import MultiObjectiveProblem
 from geneticengine.random.sources import NativeRandomSource
 from geneticengine.representations.tree.operators import FullInitializer
 from geneticengine.representations.tree.treebased import TreeBasedRepresentation
@@ -39,7 +39,31 @@ class TestUnknownObjectives:
                 fitness_function=fitness_function,
                 minimize=[],
             ),
-            budget=AnyOf(EvaluationBudget(100), TargetMultiFitness([])),
+            budget=AnyOf(EvaluationBudget(100), TargetMultiFitness([4,5])),
+            representation=TreeBasedRepresentation(g, 10),
+            random=NativeRandomSource(seed=123),
+            population_size=20,
+            population_initializer=FullInitializer(),
+            step=SequenceStep(
+                LexicaseSelection(10),
+                GenericCrossoverStep(1),
+                GenericMutationStep(1),
+            ),
+        )
+        ind = gp.search()
+        tree = ind.get_phenotype()
+        assert isinstance(tree, Root)
+        assert isinstance(tree.a, int)
+        assert tree.a > 0 and tree.b > 0
+
+    def test_unknown_objectives_and_unkown_target(self):
+        g = extract_grammar([Leaf], Root)
+        gp = GeneticProgramming(
+            problem=MultiObjectiveProblem(
+                fitness_function=fitness_function,
+                minimize=[],
+            ),
+            budget=AnyOf(EvaluationBudget(100), TargetMultiSameFitness(10)),
             representation=TreeBasedRepresentation(g, 10),
             random=NativeRandomSource(seed=123),
             population_size=20,
