f3da02bd658d5fcd6adef033ad741a0a28a4b963
Alcides Fonseca <me@alcidesfonseca.com>
2024-10-22 14:39:45 +0100

    Fixed unknown objectives test

--- tests/gp/unknown_objectives_test.py
+++ tests/gp/unknown_objectives_test.py
@@ -9,7 +9,7 @@ from geneticengine.algorithms.gp.operators.combinators import SequenceStep
 from geneticengine.algorithms.gp.operators.crossover import GenericCrossoverStep
 from geneticengine.algorithms.gp.operators.mutation import GenericMutationStep
 from geneticengine.algorithms.gp.operators.selection import LexicaseSelection
-from geneticengine.evaluation.budget import AnyOf, EvaluationBudget, TargetMultiFitness, TargetMultiSameFitness
+from geneticengine.evaluation.budget import EvaluationBudget
 from geneticengine.grammar.grammar import extract_grammar
 from geneticengine.grammar.metahandlers.ints import IntRange
 from geneticengine.problems import MultiObjectiveProblem
@@ -39,11 +39,8 @@ class TestUnknownObjectives:
         max_depth = 10
         r = NativeRandomSource(seed=123)
         gp = GeneticProgramming(
-            problem=MultiObjectiveProblem(
-                fitness_function=fitness_function,
-                minimize=[],
-            ),
-            budget=AnyOf(EvaluationBudget(100), TargetMultiFitness([4, 5])),
+            problem=MultiObjectiveProblem(fitness_function=fitness_function, minimize=[], target=[4, 5]),
+            budget=EvaluationBudget(100),
             representation=TreeBasedRepresentation(g, MaxDepthDecider(r, g, max_depth)),
             random=r,
             population_size=20,
@@ -66,11 +63,8 @@ class TestUnknownObjectives:
         g = extract_grammar([Leaf], Root)
         r = NativeRandomSource(seed=123)
         gp = GeneticProgramming(
-            problem=MultiObjectiveProblem(
-                fitness_function=fitness_function,
-                minimize=[],
-            ),
-            budget=AnyOf(EvaluationBudget(100), TargetMultiSameFitness(10)),
+            problem=MultiObjectiveProblem(fitness_function=fitness_function, minimize=[], target=[10]),
+            budget=EvaluationBudget(100),
             representation=TreeBasedRepresentation(g, MaxDepthDecider(r, g, max_depth=max_depth)),
             random=r,
             population_size=20,
