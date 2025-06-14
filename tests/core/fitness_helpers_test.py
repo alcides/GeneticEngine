21d65dbf0f916adfdcff1eb37b925384bdb716d7
Alcides Fonseca <me@alcidesfonseca.com>
2025-06-14 12:14:31 +0100

    Added test for invalidfitness

--- tests/core/fitness_helpers_test.py
+++ tests/core/fitness_helpers_test.py
@@ -7,6 +7,7 @@ from geneticengine.representations.tree.initializations import MaxDepthDecider
 from geneticengine.solutions.individual import PhenotypicIndividual
 from geneticengine.evaluation.sequential import SequentialEvaluator
 from geneticengine.problems.helpers import is_better
+from geneticengine.problems import InvalidFitnessException
 
 from geneticengine.grammar.grammar import extract_grammar
 from geneticengine.problems import SingleObjectiveProblem
@@ -40,3 +41,22 @@ class TestFitnessHelpers:
         problem = SingleObjectiveProblem(fitness_function=lambda x: x.a, minimize=False)
         [ None for _ in evaluator.evaluate(problem, [a, b])]
         assert not is_better(problem, a, b)
+
+    def test_invalid_fitness(self):
+        g = extract_grammar([Leaf], Root)
+        r = NativeRandomSource(0)
+        representation = TreeBasedRepresentation(g, MaxDepthDecider(r, g, 2))
+        evaluator = SequentialEvaluator()
+
+        a = PhenotypicIndividual(genotype=Leaf(1), representation=representation)
+        b = PhenotypicIndividual(genotype=Leaf(2), representation=representation)
+
+        def custom_fit(l:Leaf):
+            if l.a == 1:
+                raise InvalidFitnessException()
+            else:
+                return a
+
+        problem = SingleObjectiveProblem(fitness_function=custom_fit, minimize=True)
+        evaluated = [ ind for ind in evaluator.evaluate(problem, [a, b])]
+        assert len(evaluated) == 1
